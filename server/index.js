5a6a70d848c68c0521bdad1cece5e38bdda866b2
Michael Siu <michaeltsiu13@gmail.com>
2020-05-19 14:04:58 -0700

    Completed Proxy

--- server/index.js
+++ server/index.js
@@ -3,10 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
-// const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const port = 8080;
+const port = 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
